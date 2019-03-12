48eea4ccfe97bcba4de8430af223714d781f19d8
Rodrigo <gabriel.valda33@gmail.com>
2019-03-11 22:58:48 -0400

    Register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import {  LoginComponent } from './login/login.component';
 import { RegisterComponent }from './register/register.component';
 import { from } from 'rxjs';
 
 const routes: Routes = [
+  {path:'login',component:LoginComponent},
   {path:'register',component:RegisterComponent}
 ];
 
